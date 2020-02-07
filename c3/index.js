d2554249490c2aa3c566c2f9fba2bf589eb61b84
bari123 <baroademi@gmail.com>
2020-02-07 16:08:42 +0100

    express, app.get , params

--- c3/index.js
+++ c3/index.js
@@ -2,8 +2,38 @@ const express = require('express'); //callin express librarys
 
 var app = express(); //creatin express application
 
-app.get('/',(req,res)=>{
-    res.send('ok');
-}); //get method //req-request res-response
+//app.get('/',(req,res)=>{
+   // res.send('ok');
+//}); //get method //req-request res-response
+
+//app.get('/ime',(req,res)=>{
+ //   res.send('pero');
+//});
+
+//app.get('/greetings/:ime',(req,res)=>{
+ //   res.send(req.params.ime);
+//});
+app.get('/calc/:op/:a/:b',(req,res)=>{
+    let o=0
+    switch(req.params.op){
+            case 'add':
+                o =Number(req.params.a) + Number(req.params.b);
+                break;
+            case 'sub':
+                o = Number(req.params.a) - Number(req.params.b);
+                break;
+            case 'mul':
+                o = Number(req.params.a) * Number(req.params.b);
+                break;
+            case 'div':
+                o = Number(req.params.a) / Number(req.params.b);
+                break;
+    }
+    res.send(' ' + o);
+
+});
+//app.get('/calc/sub/10/4')
+//app.get('/calc/mul/2/7')
+//app.get('/calc/div/6/3')
 
 app.listen(8080);
